46448907e9268f6160ad6b9867306bd3f5e10624
zhuqitao <1362335135@qq.com>
2019-10-05 18:48:24 +0800

    test: webpack

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base.js')
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, '/Users/zhuqitao/develop/webpack/chap04/test/test/smoke/template/src/index/index.js');
-        assert.equal(baseConfig.entry.search, '/Users/zhuqitao/develop/webpack/chap04/test/test/smoke/template/src/search/index.js');
+        assert.equal(baseConfig.entry.index.includes('smoke/template/src/index/index.js'), true);
+        assert.equal(baseConfig.entry.search.includes('smoke/template/src/search/index.js'), true);
     });
 });
